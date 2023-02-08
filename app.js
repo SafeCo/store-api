71c901f3a7edab7ff91e1068f44649a68cf55ba3
SafeCo <77068178+SafeCo@users.noreply.github.com>
2023-02-08 14:37:21 +0000

    added server logic
    added server logic and middleware

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("working")
+require("dotenv").config()
+
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+app.use(express.json())
+
+app.get('/', (re,res)=>{
+    res.send('<h1>Store Api</h1><a href="api/v1/products">products route</a>')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+    try {
+        app.listen(port, console.log(`server is listening to port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
