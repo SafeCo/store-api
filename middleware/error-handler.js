6585d8652da9d4e67b2e4b62050348f5c6a4cc13
SafeCo <77068178+SafeCo@users.noreply.github.com>
2023-05-23 03:22:09 +0100

    error handler middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,6 @@
-const {CustomApiError} = require('../errors/custom-error')
 
 const errorHandlerMiddleware = (err, req, res, next) =>{
-    if(err instanceof CustomApiError){
-        return res.status(err.status).json({msg: err.message})
-    }
+    console.log(err)
     return res.status(500).json({msg : "Something went wrong, please try again"})
 }
 
